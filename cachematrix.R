149bc39aea9af29a0452af9f7d24eca08faadc62
harry123mor <tara_r_a@hotmail.co.uk>
2016-07-29 12:32:56 +0100

    programming assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function ff
+
+makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(h) {
+                x <<- h
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) i <<- solve
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
+}
